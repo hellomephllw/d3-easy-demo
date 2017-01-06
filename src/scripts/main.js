f24bfbfead7ee6e4dde38920c64ef5f1cf021dc1
llw <hellomephllw@163.com>
2017-01-06 17:48:41 +0800

    test some easy things

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,9 @@
 import * as d3 from 'd3';
 
+let pEle = d3.select('#p');
+
+d3.selectAll('div').filter('.abc').classed('abc bbb');
+
+let myclass = d3.selectAll('div').filter('.abc').classed('abc');
+
+console.log(myclass);
